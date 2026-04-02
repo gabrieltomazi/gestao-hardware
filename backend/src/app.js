54514770a15b0a0823e75f83dce48aa5a2e40797
gabrieltomazi <tomazigabriel3@gmail.com>
2026-04-02 14:59:57 -0300

    chore(api): configure CORS for production deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,11 @@ import routes from "./routes/index.js";
 import cors from 'cors'
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: "https://gestao-hardware.vercel.app/",
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use(express.json())
 app.use(routes)
 
